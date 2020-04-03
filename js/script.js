9a1357e890713fd4846957ef5d2f6971e5ea02c8
beingbiplov <beingbiplov@gmail.com>
2020-04-03 19:22:44 +0545

    setting up showdown

--- js/script.js
+++ js/script.js
@@ -4,9 +4,35 @@ var myCodeMirror = CodeMirror(document.getElementById('editor_section'), {
 			lineNumbers: true,
 			lineWrapping: true,
 			spellcheck: true,
-		 	mode:  "javascript",
+		 	mode:  "markdown",
 		    highlightFormatting: true,
 		    styleActiveLine: true,
 		    autoCloseBrackets: true,
 		    highlightSelectionMatches: {showToken: /\w/, annotateScrollbar: true},
 		});
+
+
+function markdown_to_html() {
+	showdown.setOption('noHeaderId' , true);
+  	var text =  myCodeMirror.getValue(),
+      
+	converter = new showdown.Converter({
+					tables: true, 
+					strikethrough: true,
+					ghCodeBlocks: true,
+					simplifiedAutoLink: true,
+					smoothLivePreview: true,
+					simpleLineBreaks: true,
+					requireSpaceBeforeHeadingText: true,
+				}),
+	html = converter.makeHtml(text);
+    
+    return html;
+}
+
+
+$(document).ready(function(){
+	$('#editor_section').keyup(function(){
+		$('#preview_section').html(markdown_to_html());
+	})
+})
