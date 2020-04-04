1299ad85430221d4eca7c6c52fe5b59eeb81c893
beingbiplov <beingbiplov@gmail.com>
2020-04-04 11:27:09 +0545

    Adding themes to the editor

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ var myCodeMirror = CodeMirror(document.getElementById('editor_section'), {
 		    styleActiveLine: true,
 		    autoCloseBrackets: true,
 		    highlightSelectionMatches: {showToken: /\w/, annotateScrollbar: true},
+		    
 		});
 
 
@@ -36,3 +37,22 @@ $(document).ready(function(){
 		$('#preview_section').html(markdown_to_html());
 	})
 })
+
+
+var input = document.getElementById("select");
+function selectTheme() {
+    var theme = input.value;
+    myCodeMirror.setOption("theme", theme);
+    
+  }
+  var choice = (location.hash && location.hash.slice(1)) ||
+               (document.location.search &&
+                decodeURIComponent(document.location.search.slice(1)));
+  if (choice) {
+    input.value = choice;
+    myCodeMirror.setOption("theme", choice);
+  }
+  CodeMirror.on(window, "hashchange", function() {
+    var theme = location.hash.slice(1);
+    if (theme) { input.value = theme; selectTheme(); }
+  });
